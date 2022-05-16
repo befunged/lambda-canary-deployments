4e6de5535333a48db1fa67b790f8b3bc2a92cd1f
Andy Drummond <andy.drummond@sky.uk>
2022-05-16 16:15:21 +0100

    start throwing 500s after some 200 responses

--- functions/apiHandler.ts
+++ functions/apiHandler.ts
@@ -1,12 +1,25 @@
 import {ProxyHandler} from 'aws-lambda'
 
+let counter: number = 0;
+
 export const handler: ProxyHandler = async (event) => {
-  return {
-    body: JSON.stringify({
-      message: 'API version 5 has been deployed!',
-      path: event.path,
-    }),
-    headers: { 'Content-Type': 'application/json' },
-    statusCode: 200,
-  }
+
+    let statusCode: number = 200;
+    let message: string = 'API version 6 has been deployed!'
+
+    if (counter > 10) {
+        statusCode = 500;
+        message = 'API version 6 has been deployed! : Throwing 500'
+    }
+
+    counter++;
+
+    return {
+        body: JSON.stringify({
+            message: message,
+            path: event.path,
+        }),
+        headers: {'Content-Type': 'application/json'},
+        statusCode: statusCode,
+    }
 }
