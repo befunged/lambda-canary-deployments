9958a7b1ca45379d3c2a4b3744a63025734208ae
Andy Drummond <andy.drummond@sky.uk>
2022-05-20 15:47:44 +0100

    throw error

--- README.md
+++ README.md
@@ -14,3 +14,6 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 - `yarn cdk deploy` deploy this stack to your default AWS account/region
 - `yarn cdk diff` compare deployed stack with current state
 - `yarn cdk synth` emits the synthesized CloudFormation template
+
+
+dummy to cause deploy
--- functions/apiHandler.ts
+++ functions/apiHandler.ts
@@ -2,9 +2,11 @@ import {ProxyHandler} from 'aws-lambda'
 
 export const handler: ProxyHandler = async (event) => {
 
+    throw new Error();
+
     return {
         body: JSON.stringify({
-            message: 'API version 12',
+            message: 'API version 13',
             path: event.path,
         }),
         headers: {'Content-Type': 'application/json'},
